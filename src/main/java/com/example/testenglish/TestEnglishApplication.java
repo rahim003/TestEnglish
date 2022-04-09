62b1c1c04f864ab974bec1ce865c41633bf9f3ed
Aisuluu <intellijei.123@gmail.com>
2022-04-09 11:09:04 +0600

    Test

---
+++ src/main/java/com/example/testenglish/TestEnglishApplication.java
@@ -0,0 +1,13 @@
+package com.example.testenglish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestEnglishApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestEnglishApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testenglish/TestEnglishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testenglish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestEnglishApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
