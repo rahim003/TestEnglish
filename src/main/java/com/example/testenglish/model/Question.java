880f8e79885b70265e1c23f6a731449c7f2a066c
Aisuluu <intellijei.123@gmail.com>
2022-04-09 12:08:49 +0600

    create

--- src/main/java/com/example/testenglish/model/Question.java
+++ src/main/java/com/example/testenglish/model/Question.java
@@ -1,5 +1,6 @@
 package com.example.testenglish.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ import static javax.persistence.CascadeType.*;
 
 @Entity
 @NoArgsConstructor
+@AllArgsConstructor
 @Data
 @Table(name = "questions")
 public class Question {
